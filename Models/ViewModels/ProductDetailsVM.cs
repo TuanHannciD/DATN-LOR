0a16fff0fbf07764d56e7bf3a8b41d14f3a26ea4
T1HannCid <doba2311@gmail.com>
2025-08-09 21:58:54 +0700

    Kiểm tra và merge qua view

---
+++ Models/ViewModels/ProductDetailsVM.cs
@@ -0,0 +1,17 @@
+﻿using AuthDemo.Models;
+
+namespace AuthDemo.Models.ViewModels
+{
+    public class ProductDetailsVM
+    {
+        public Giay giay { get; set; }
+        public ChiTietGiay ctgiay   { get; set; }
+        public List<MauSac> MauSacOptions { get; set; }
+        public List<string> DanhSachAnh { get; set; }
+        public List<AnhGiay> anhgiay { get; set; }
+        public List<KichThuoc> KichCoOptions { get; set; }
+        public List<ThuongHieu> ThuongHieuOptions { get; set; }
+
+        public List<SanPhamLqVM> SanPhamLienQuan { get; set; }
+    }
+}
---
+++ Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AuthDemo.Models;
+
+namespace AuthDemo.Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Guid ShoeID { get; set; }
+        public string TenGiay { get; set; }
+        public string AnhDaiDien { get; set; }
+        public decimal GiaThapNhat { get; set; }
+        public Guid CategoryID { get; set; }        
+        public Guid SizeID { get; set; }       
+        public Guid ColorID { get; set; }         
+        public Guid MaterialID { get; set; } 
+        public Guid BrandID { get; set; } 
+        public int SoLuong { get; set; }
+    }
+}
