fa947ec792744c444f379faa3df6ea6193133241
T1HannCid <doba2311@gmail.com>
2025-08-19 04:34:09 +0700

    Chuẩn bị cho thêm và api gắn link ảnh

--- Models/ViewModels/ChiTietGiayVM.cs
+++ Models/ViewModels/ChiTietGiayVM.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AuthDemo.Models.ViewModels
 {
@@ -34,7 +35,7 @@ namespace AuthDemo.Models.ViewModels
         // ViewModel cho Edit
         public class EditVM
         {
-            public string ?TenGiay { get; set; }
+            public string? TenGiay { get; set; }
             public Guid ShoeDetailID { get; set; }
             public Guid ShoeID { get; set; }
             public Guid SizeID { get; set; }
@@ -44,6 +45,8 @@ namespace AuthDemo.Models.ViewModels
             public Guid CategoryID { get; set; }
             public int SoLuong { get; set; }
             public decimal Gia { get; set; }
+            [Required]
+            public string Image { get; set; }
         }
     }
 } 
