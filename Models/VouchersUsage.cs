4984a98cb63d50d205fa6b0743ebe0c8af790f3a
nguyenxuanan1 <xuanannguyen1603@gmail.com>
2025-08-27 01:41:52 +0700

    thêm voucher,voucherusage,và 2 config

---
+++ Models/VouchersUsage.cs
@@ -0,0 +1,16 @@
+﻿namespace AuthDemo.Models
+{
+    public class VouchersUsage :ThongTinChung
+    {
+        public Guid VoucherUsageID { get; set; } = Guid.NewGuid();
+
+        public Guid VoucherID { get; set; }
+        public Guid UserID { get; set; }
+        public Guid BillID { get; set; }   // Lưu đơn hàng nào đã dùng voucher này
+
+        // Navigation
+        public Vouchers Vouchers { get; set; }
+        public NguoiDung NguoiDung { get; set; }
+        public HoaDon HoaDon { get; set; }
+    }
+}
