ae56ac5503b8da44a14da30ab0a4344bb1c6bca4
T1HannCid <doba2311@gmail.com>
2025-09-05 00:24:22 +0700

    Format lại

--- wwwroot/js/validate.js
+++ wwwroot/js/validate.js
@@ -1,33 +1,33 @@
 function validateForm(form) {
-    let valid = true;
-    const inputs = form.querySelectorAll("[data-validate]");
+  let valid = true;
+  const inputs = form.querySelectorAll("[data-validate]");
 
-    inputs.forEach(input => {
-        const rules = input.dataset.validate.split("|");
-        const value = input.value.trim();
-        const labelName = input.dataset.label || "Trường này";
+  inputs.forEach((input) => {
+    const rules = input.dataset.validate.split("|");
+    const value = input.value.trim();
+    const labelName = input.dataset.label || "Trường này";
 
-        rules.forEach(rule => {
-            if (rule === "required" && value === "") {
-                showToast(`${labelName} không được để trống!`, false);
-                valid = false;
-            }
-            if (rule.startsWith("min:")) {
-                const minLen = parseInt(rule.split(":")[1]);
-                if (value.length < minLen) {
-                    showToast(`${labelName} phải ít nhất ${minLen} ký tự!`, false);
-                    valid = false;
-                }
-            }
-            if (rule.startsWith("minValue:")) {
-                const minVal = parseFloat(rule.split(":")[1]);
-                if (parseFloat(value) < minVal) {
-                    showToast(`${labelName} phải lớn hơn hoặc bằng ${minVal}`,false);
-                    valid = false;
-                }
-            }
-        });
+    rules.forEach((rule) => {
+      if (rule === "required" && value === "") {
+        showToast(`${labelName} không được để trống!`, false);
+        valid = false;
+      }
+      if (rule.startsWith("min:")) {
+        const minLen = parseInt(rule.split(":")[1]);
+        if (value.length < minLen) {
+          showToast(`${labelName} phải ít nhất ${minLen} ký tự!`, false);
+          valid = false;
+        }
+      }
+      if (rule.startsWith("minValue:")) {
+        const minVal = parseFloat(rule.split(":")[1]);
+        if (parseFloat(value) < minVal) {
+          showToast(`${labelName} phải lớn hơn hoặc bằng ${minVal}`, false);
+          valid = false;
+        }
+      }
     });
+  });
 
-    return valid;
+  return valid;
 }
