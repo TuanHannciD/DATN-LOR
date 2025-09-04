9c40794fcd4845b713406c697028391f849fea1f
T1HannCid <doba2311@gmail.com>
2025-09-05 00:25:32 +0700

    Thêm chức năng thêm nhanh khách hàng

--- Areas/Admin/Interface/IBanHangTaiQuayService.cs
+++ Areas/Admin/Interface/IBanHangTaiQuayService.cs
@@ -3,12 +3,14 @@ using AuthDemo.Models.ViewModels;
 using System;
 using AuthDemo.Models;
 using AuthDemo.Common;
+using CloudinaryDotNet;
 
 namespace AuthDemo.Areas.Admin.Interface
 {
     public interface IBanHangTaiQuayService
     {
         List<BanHangTaiQuayVM> SearchSanPham(string keyword);
+        Task<ApiResponse<QuickAddCustomerVM>> CreateKhachHang(QuickAddCustomerVM model);
         List<KhachHangDropdownVM> SearchKhachHang(string keyword);
         List<CartItemDisplayVM> GetCartItems(string tenDangNhap);
         ApiResponse<string> UpdateCart(string tenDangNhap, Guid shoeDetailId, string actionType);
